28b23f0b6580c9f39a8bc02b95e37b76a48629a7
Shanna Sullivan <shanna.sullivan@gmail.com>
2015-11-27 09:32:46 -0700

    google map showing up on localhost server. removed api key

---
+++ app.js
@@ -0,0 +1,9 @@
+var map;
+function initMap() {
+	// create a map object and specify the DOM element for display
+	map = new google.maps.Map(document.getElementById('map'), {
+		center: {lat: 37.332, lng: -121.889},
+		scrollwheel: false,
+		zoom: 15
+	});
+}
