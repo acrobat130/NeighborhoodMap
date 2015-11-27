57e791bd2c7fc98a733393c945f4edb92f40b1aa
Shanna Sullivan <shanna.sullivan@gmail.com>
2015-11-27 09:36:47 -0700

    tweaked center lat/lng in initMap function

--- app.js
+++ app.js
@@ -2,7 +2,7 @@ var map;
 function initMap() {
 	// create a map object and specify the DOM element for display
 	map = new google.maps.Map(document.getElementById('map'), {
-		center: {lat: 37.332, lng: -121.889},
+		center: {lat: 37.332, lng: -121.893},
 		scrollwheel: false,
 		zoom: 15
 	});
